87cb677c71b946dcb77b2ff8643acec7c4d6d5b4
N <012345@mail.ua>
2015-10-14 01:48:33 +0300

    виконане 1 і 3 завдання

--- task1.php
+++ task1.php
@@ -1,12 +1,15 @@
 <?php
 header("Content-Type: text/html; charset=utf-8");
+header("X-XSS-Protection: 0");
+session_start();
+$admin_login = 'admin';
+$admin_password = '012345';
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
 	<meta charset="UTF-8">
-	<meta http-equiv="refresh" content="1">
 	<script src="js/jquery-1.11.3.min.js"></script>
 	<script src="js/bootstrap.min.js"></script>
 	<link rel="stylesheet" type="text/css" href="style/bootstrap.min.css">
@@ -20,6 +23,37 @@ header("Content-Type: text/html; charset=utf-8");
 		<a class="btn btn-default btn-block" href="task1.php">завдання 1</a>
 	</nav>
 
+	<div class="container">
+
+		<?php
+
+			if (isset($_POST['login']) && isset($_POST['password'])) {
+				if ($_POST['login'] == $admin_login && $_POST['password'] == $admin_password) {
+					$_SESSION['login'] = $_POST['login'];
+					
+				}
+				else {
+					print 'Wrong username or password';
+				}
+			}
+
+			if (!isset($_SESSION['login'])) {
+				print '<form  class="form-inline " role="form" action="task1.php" method="POST">
+						<div class="form-group">
+							<input class="form-control" type="text" name="login" placeholder="login: admin" required>
+							<input class="form-control" type="password" name="password" placeholder="password: 012345" required>
+							
+							<input class="btn btn-danger" type="submit" name="submit" value="log in">
+						<div>
+						</form>' ;
+			}
+			else {
+				print '<h3>Welcome!</h3><p><a href="session-destroy.php">log out</a>';
+			}
+
+		?>
+
+	</div>
 	
 </body>
 </html>
